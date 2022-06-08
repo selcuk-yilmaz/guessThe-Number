e6b28d27416844cb9b48611d14da01b376a6444a
selcuklu <yilmax4@gmail.com>
2022-06-08 12:24:16 +0300

    setted

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+let start=0;
+let stop=100;
 let x = Math.floor(Math.random() * 101);
 // console.log(x);
 document.querySelector(".but").addEventListener("click", () => {
@@ -7,15 +8,18 @@ document.querySelector(".but").addEventListener("click", () => {
   if (input == x) {
     document.querySelector("#img").src =
       "https://i.pinimg.com/474x/c5/82/01/c582011acd8b6095cccce7d06dd78173.jpg";
+     document.querySelector(".brif").innerHTML= `🎇Congrats! lucky number is ${input}🎇`; 
       
   } else if (input < x) {
+    start = input;
+     document.querySelector(
+      ".brif"
+    ).innerHTML = `Please enter a number between  ${start}  and ${stop} `;
+  } else if (input > x) { 
+     stop=input;
     document.querySelector(
       ".brif"
-    ).innerHTML = `please enter anumber between ${input} and 100`;
-  } else if (input > x) {
-    document.querySelector(
-      ".brif"
-    ).innerHTML = `please enter a number between  0 and  ${input}  `;
+    ).innerHTML = `Please enter a number between  ${start} and  ${stop}`;
   }
   document.querySelector("#num").value = "";
 });
